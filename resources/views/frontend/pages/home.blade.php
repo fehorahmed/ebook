1135973bfc0c2fb42cd1a8ce5a5500a2124ec0d4
ahmedfehor <ahmedfehor@gmail.com>
2025-02-02 15:25:38 +0600

    okk

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -6,14 +6,16 @@
 
 @section('styles')
     <style>
-        .card-body{
-            min-height:0px;
+        .card-body {
+            min-height: 0px;
 
         }
-        .card_height{
+
+        .card_height {
             height: 365px;
         }
-        .card_size{
+
+        .card_size {
             height: 65px;
         }
 
@@ -67,104 +69,115 @@
                 display: block;
             }
         }
-
     </style>
 @endsection
 
 @section('main-content')
-<main class="main">
-    <section class="new-products-section">
-        <div class="container">
-            <h4>সাম্প্রতিক বই</h4>
-            <script>
-                {{ $adShow->home_page_ad_one ?? '' }}
-            </script>
-           <div class="row">
-                @foreach ($books as $book)
-                    <div class="col-md-2">
-                        <a href="{{ route('book_details', ($book->slug)) }}">
-                            <div class="card shadow card_height" style="width: 18rem;">
-                                <img src="{{ (!empty($book->image)) ? asset('upload/book_image/'. $book->image) : null }}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
+    <main class="main">
+        <section class="new-products-section">
+            <div class="container">
+                <h4>সাম্প্রতিক বই</h4>
+                <script>
+                    {{ $adShow->home_page_ad_one ?? '' }}
+                </script>
+                <div class="row">
+                    @foreach ($books as $book)
+                        <div class="col-md-2">
+                            <a href="{{ route('book_details', $book->slug) }}">
+                                <div class="card shadow card_height" style="width: 18rem;">
+                                    <img src="{{ !empty($book->image) ? asset('upload/book_image/' . $book->image) : null }}"
+                                        class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-           </div>
-        </div>
-        {!! $adShow->writer_page_ad_one ?? '' !!}
-        <div class="container">
-            <h4>জনপ্রিয় বই</h4>
-           <div class="row">
-                @foreach ($popularBooks as $book)
-                    <div class="col-md-2">
-                        <a href="{{ route('book_details', ($book->slug)) }}">
-                            <div class="card shadow card_height" style="width: 18rem;">
-                                <img src="{{ (!empty($book->image)) ? asset('upload/book_image/'. $book->image) : null }}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-           </div>
-        </div>
-        {!! $adShow->home_page_ad_three ?? '' !!}
-        <div class="container">
-            <h4>সেবা প্রকাশনী</h4>
-           <div class="row">
-                @foreach ($categoryBooks as $book)
-                    <div class="col-md-2">
-                        <a href="{{ route('book_details', ($book->slug)) }}">
-                            <div class="card shadow card_height" style="width: 18rem;">
-                                <img src="{{ (!empty($book->image)) ? asset('upload/book_image/'. $book->image) : null }}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-           </div>
-        </div>
-        {!! $adShow->home_page_ad_three ?? '' !!}
-        <div class="container">
-            <h4>ইসলামিক বই</h4>
-           <div class="row">
-                @foreach ($islamicBooks as $book)
-                    <div class="col-md-2">
-                        <a href="{{ route('book_details', ($book->slug)) }}">
-                            <div class="card shadow card_height" style="width: 18rem;">
-                                <img src="{{ (!empty($book->image)) ? asset('upload/book_image/'. $book->image) : null }}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-           </div>
-        </div>
-        {!! $adShow->home_page_ad_three ?? '' !!}
-        <div class="container">
-            <h4>বইয়ের ধরণ</h4>
-            <div class="row mt-5">
-                @foreach ($categories as $category)
-                    <div class="col-lg-3">
-                        <div class="card card_size shadow">
-                           <a href="{{ route('category_wise_book', encrypt($category->id)) }}">
-                                <div class="card-body">
-                                    <h5 class="card-title text-center">{{ $category->name ?? '' }}</h5>
-                                </div>
-                           </a>
+                            </a>
                         </div>
-                    </div><!-- End .col-lg-3 -->
-                @endforeach
-            </div><!-- End .row -->
-        </div><!-- End .container -->
+                    @endforeach
+                </div>
+            </div>
+            <div class="row">
+                {!! $adShow->writer_page_ad_one ?? '' !!}
+            </div>
+            <div class="container">
+                <h4>জনপ্রিয় বই</h4>
+                <div class="row">
+                    @foreach ($popularBooks as $book)
+                        <div class="col-md-2">
+                            <a href="{{ route('book_details', $book->slug) }}">
+                                <div class="card shadow card_height" style="width: 18rem;">
+                                    <img src="{{ !empty($book->image) ? asset('upload/book_image/' . $book->image) : null }}"
+                                        class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="row">
+                {!! $adShow->home_page_ad_three ?? '' !!}
+            </div>
+            <div class="container">
+                <h4>সেবা প্রকাশনী</h4>
+                <div class="row">
+                    @foreach ($categoryBooks as $book)
+                        <div class="col-md-2">
+                            <a href="{{ route('book_details', $book->slug) }}">
+                                <div class="card shadow card_height" style="width: 18rem;">
+                                    <img src="{{ !empty($book->image) ? asset('upload/book_image/' . $book->image) : null }}"
+                                        class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="row">
+                {!! $adShow->home_page_ad_three ?? '' !!}
+            </div>
+            <div class="container">
+                <h4>ইসলামিক বই</h4>
+                <div class="row">
+                    @foreach ($islamicBooks as $book)
+                        <div class="col-md-2">
+                            <a href="{{ route('book_details', $book->slug) }}">
+                                <div class="card shadow card_height" style="width: 18rem;">
+                                    <img src="{{ !empty($book->image) ? asset('upload/book_image/' . $book->image) : null }}"
+                                        class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <p class="card-text">{{ $book->name ?? '' }}-{{ $book->writer->name ?? '' }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="row">
+                {!! $adShow->home_page_ad_three ?? '' !!}
+            </div>
+            <div class="container">
+                <h4>বইয়ের ধরণ</h4>
+                <div class="row mt-5">
+                    @foreach ($categories as $category)
+                        <div class="col-lg-3">
+                            <div class="card card_size shadow">
+                                <a href="{{ route('category_wise_book', encrypt($category->id)) }}">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">{{ $category->name ?? '' }}</h5>
+                                    </div>
+                                </a>
+                            </div>
+                        </div><!-- End .col-lg-3 -->
+                    @endforeach
+                </div><!-- End .row -->
+            </div><!-- End .container -->
 
             {!! $adShow->home_page_ad_two ?? '' !!}
 
@@ -172,10 +185,9 @@
 
             {!! $adShow->home_page_ad_five ?? '' !!}
 
-    </section>
-</main>
+        </section>
+    </main>
 @endsection
 
 @section('scripts')
-
 @endsection
